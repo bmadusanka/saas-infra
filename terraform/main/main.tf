fed71c741474a2bc23252bcd34ce7b3657706f0d
bmadusanka <madusankavpb@gmail.com>
2026-05-19 11:04:05 +0200

    Adding backend

--- terraform/main/main.tf
+++ terraform/main/main.tf
@@ -6,9 +6,6 @@ module "labels" {
   project        = var.project
   stage          = var.stage
   layer          = var.stage
-  project_id     = var.project_id
-  kst            = var.tag_KST
-  wa_number      = var.wa_number
   resources = [
     "consultation-app"
   ]
