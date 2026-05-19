0fc99839bd09613c3c1ce836545e41430c0c1af8
Bhagya Madusanka <madusankavpb@gmail.com>
2026-05-19 18:23:00 +0200

    Update secrets_manager

--- terraform/main/secrets_manager.tf
+++ terraform/main/secrets_manager.tf
@@ -9,3 +9,18 @@ resource "aws_secretsmanager_secret" "clerk_url" {
 resource "aws_secretsmanager_secret" "clerk_key" {
   name = "clerk-secret-key"
 }
+
+resource "aws_secretsmanager_secret_version" "openai" {
+  secret_id     = aws_secretsmanager_secret.openai.id
+  secret_string = var.OPENAI_API_KEY
+}
+
+resource "aws_secretsmanager_secret_version" "clerk_url" {
+  secret_id     = aws_secretsmanager_secret.clerk_url.id
+  secret_string = var.CLERK_JWKS_URL
+}
+
+resource "aws_secretsmanager_secret_version" "clerk_key" {
+  secret_id     = aws_secretsmanager_secret.clerk_key.id
+  secret_string = var.CLERK_SECRET_KEY
+}
